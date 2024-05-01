9ca528acffef8bd85b2e1a327028d6f9b6bb52eb
Ivan Flores <ifloresmanc96@gmail.com>
2024-04-30 21:32:55 -0300

    install retrofit and gson converter, ahora si

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.compose)
     implementation("io.coil-kt:coil-compose:2.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
