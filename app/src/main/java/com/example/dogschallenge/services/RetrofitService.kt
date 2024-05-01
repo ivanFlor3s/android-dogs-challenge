f67bebf607c0c2c876f5cbd3c39a0d9a65810ce2
Ivan Flores <ifloresmanc96@gmail.com>
2024-04-30 21:33:47 -0300

    add retro fit interface and object to instance (TODO preferable as DI)

--- app/src/main/java/com/example/dogschallenge/services/RetrofitService.kt
+++ app/src/main/java/com/example/dogschallenge/services/RetrofitService.kt
@@ -1,4 +1,25 @@
 package com.example.dogschallenge.services
 
-class RetrofitService {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface RetrofitService {
+    @GET("breeds/list/all")
+    suspend fun listBreeds()
+    @GET("breed/{breed}/images/random")
+    suspend fun getRandomImageByBreed(
+        @Path("breed") breed: String
+    )
+}
+
+object RetrofitServiceFactory {
+    fun create(): RetrofitService {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RetrofitService::class.java)
+    }
 }
